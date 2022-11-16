8de9e2063908cda8937f1a2fb8f7d9da2c6a26d7
islamdev96 <islamsayedglab@gmail.com>
2022-11-16 03:59:48 +0200

    00

--- lib/ui/shared/new/carousel_slider.dart
+++ lib/ui/shared/new/carousel_slider.dart
@@ -13,36 +13,41 @@ class CarouselSlider2 extends StatelessWidget {
         //1st Image of Slider
 
         ContainerImageWidget(
-          image: 'assets/images/islam/10.jpg',
+          image: 'assets/images/islam/islam1.jpg',
         ),
         ContainerImageWidget(
-          image: 'assets/images/islam/9.jpg',
+          image: 'assets/images/islam/islam2.jpg',
         ),
         ContainerImageWidget(
-          image: 'assets/images/islam/6.jpg',
+          image: 'assets/images/islam/islam3.png',
         ),
         ContainerImageWidget(
-          image: 'assets/images/islam/1.jpg',
+          image: 'assets/images/islam/islam4.jpg',
         ),
         ContainerImageWidget(
-          image: 'assets/images/islam/3.jpg',
+          image: 'assets/images/islam/islam5.jpg',
         ),
         ContainerImageWidget(
-          image: 'assets/images/islam/7.jpg',
+          image: 'assets/images/islam/islam6.jpg',
         ),
         ContainerImageWidget(
-          image: 'assets/images/islam/3.jpg',
+          image: 'assets/images/islam/islam7.jpg',
+        ),
+        ContainerImageWidget(
+          image: 'assets/images/islam/islam8.jpg',
         ),
       ],
 
-      //Slider Container properties
+      // Slider Container properties
       options: CarouselOptions(
-        height: MediaQuery.of(context).size.height * 0.4,
+        height: MediaQuery.of(context).size.height * 0.55,
+        scrollDirection: Axis.horizontal,
         enlargeCenterPage: true,
         autoPlay: true,
         aspectRatio: 16 / 9,
         autoPlayCurve: Curves.fastOutSlowIn,
         enableInfiniteScroll: true,
+        // autoPlayInterval: const Duration(seconds: 1),
         autoPlayAnimationDuration: const Duration(milliseconds: 3000),
         viewportFraction: 0.8,
       ),
